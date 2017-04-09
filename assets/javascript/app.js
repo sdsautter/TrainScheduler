2a6ed94e3cbd36333312a7e320dafbd35fdc8f57
sdsautter <sdsautter@gmail.com>
2017-04-09 15:46:51 -0400

    Functional but not stylish

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,96 +19,46 @@ $(function() {
             time: $("#form-time").val().trim(),
             frequency: $("#form-frequency").val().trim()
         })
-        $("#form-*").val('');
+        $("#form-name").val('');
+        $("#form-destination").val('');
+        $("#form-time").val('');
+        $("#form-frequency").val('');
 
     })
     database.ref().on("child_added", function(snapshot) {
-        let newTrain = snapshot.val();
-        let tr = $('<tr>');
-        let td = $('<td>');
+        var newTrain = snapshot.val();
+        var tr = $('<tr>');
+        var td = $('<td>');
 
 
         var tFrequency = newTrain.frequency;
 
-        // Time is 3:30 AM
         var firstTime = newTrain.time;
 
         // First Time (pushed back 1 year to make sure it comes before current time)
         var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-        console.log(firstTimeConverted);
 
         // Current Time
         var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
         // Difference between the times
         var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
 
         // Time apart (remainder)
         var tRemainder = diffTime % tFrequency;
-        console.log(tRemainder);
 
         // Minute Until Train
         var tMinutesTillTrain = tFrequency - tRemainder;
-        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
         // Next Train
         var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-        var trainTime = moment(newTrain.time).format("YYYY, MM, DD");
-
-
-
-
-
-        // let nextTrain = parseInt(newTrain.frequency) * trainMonths;
 
 
         tr.append(td.clone().text(newTrain.name));
         tr.append(td.clone().text(newTrain.destination));
-        tr.append(td.clone().text(newTrain.time));
-        tr.append(td.clone().text(trainMonths));
         tr.append(td.clone().text(newTrain.frequency));
-        tr.append(td.clone().text(trainTotal));
+        tr.append(td.clone().text(moment(nextTrain).format("hh:mm A")));
+        tr.append(td.clone().text(tMinutesTillTrain));
         $("tbody").append(tr);
     })
 })
-
-
-
-// Assume the following situations.
-
-// (TEST 1)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 3 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:18 -- 2 minutes away
-
-// (TEST 2)
-// First Train of the Day is 3:00 AM
-// Assume Train comes every 7 minutes.
-// Assume the current time is 3:16 AM....
-// What time would the next train be...? (Use your brain first)
-// It would be 3:21 -- 5 minutes away
-
-
-// ==========================================================
-
-// Solved Mathematically
-// Test case 1:
-// 16 - 00 = 16
-// 16 % 3 = 1 (Modulus is the remainder)
-// 3 - 1 = 2 minutes away
-// 2 + 3:16 = 3:18
-
-// Solved Mathematically
-// Test case 2:
-// 16 - 00 = 16
-// 16 % 7 = 2 (Modulus is the remainder)
-// 7 - 2 = 5 minutes away
-// 5 + 3:16 = 3:21
-
-// Assumptions
